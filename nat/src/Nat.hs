cd9440678784462ce5f436a818085c048e1b0b1c
Liu Xinyu <liuxinyu95@gmail.com>
2018-05-26 08:46:44 +0800

    Defiend mul

--- nat/src/Nat.hs
+++ nat/src/Nat.hs
@@ -1,9 +1,16 @@
 module Nat where
 
-import Prelude hiding ((+))
+import Prelude hiding ((+), (*))
 
 data Nat = Zero | Succ Nat deriving Show
 
 (+) :: Nat -> Nat -> Nat
-m + Zero = m
-m + (Succ n) = Succ (m + n)
+a + Zero = a
+a + (Succ b) = Succ (a + b)
+
+(*) :: Nat -> Nat -> Nat
+a *  Zero = Zero
+a * (Succ b) = a * b + a
+
+foldn z _ Zero = Zero
+foldn z f (Succ n) = f (foldn z f n)
