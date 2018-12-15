c830f2a461b76c5eea06d64e4982cbae32dec7b9
Xinyu Liu <liuxinyu95@gmail.com>
2018-12-15 08:14:47 +0800

    Added big number unit table

--- deduction/src/ThreeSons.hs
+++ deduction/src/ThreeSons.hs
@@ -1,6 +1,6 @@
 sol = [(x, y, z, n) |
          x <- [2, 3],
          y <- [(x + 1) .. 6],
-         z <- [(y + 1) .. 12], x * y * z - y * z - x * z - x * y /= 0,
-         n <- [(y * z + x * z + x * y) `div` (x * y * z - y * z - x * z - x * y)],
-         (n + 1) `div` x + (n + 1) `div` y + (n + 1) `div` z == n, n > 0]
+         z <- [(y + 1) .. 12], let d = x * y * z - y * z - x * z - x * y, d > 0,
+         let n = (y * z + x * z + x * y) `div` d,
+         (n + 1) `div` x + (n + 1) `div` y + (n + 1) `div` z == n]
