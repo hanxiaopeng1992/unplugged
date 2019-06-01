885c4fff1bcec5360a65a5a8135f0e259eb1df28
Xinyu Liu <liuxinyu95@gmail.com>
2019-06-01 23:14:04 +0800

    Added answer to exercise ()^m

--- nat/src/NatN.hs
+++ nat/src/NatN.hs
@@ -7,3 +7,16 @@ fib = fst . foldn (0, 1) h where
   h (m, n) = (n, m + n)
 
 --take 10 $ map fst $ iterate (\(m, n) -> (n, m + n)) (0, 1)
+
+-- m + ()
+plus m = foldn m (+1)
+
+-- m * ()
+times m = foldn 0 (+m)
+
+-- m^()
+power m = foldn 1 (*m)
+
+-- ()^m
+expo m = snd . foldn (0, 0) h where
+  h (i, b) = (i + 1, power (i + 1) m)
