684fcd870ae702d1ce31053f33e574f4e43cc147
Xinyu LIU <liuxinyu95@gmail.com>
2020-05-01 08:49:08 +0800

    Added AI Tic-Tac-Toe program with Luo Shu Magic Square

---
+++ appendix/preface/src/tictactoe.py
@@ -0,0 +1,110 @@
+# Tic-Tac-Toe game, use min-max method.
+
+WIN = 10
+INF = 1000
+
+# Luo Shu magic square
+MAGIC_SQUARE = [4, 9, 2,
+                3, 5, 7,
+                8, 1, 6]
+
+def eval(x, o):
+    if win(x):
+        return WIN
+    if win(o):
+        return -WIN
+    return 0
+
+# check if any 3 numbers (triplet) add to 15.
+# There are 2 methods:
+#   (1) two pointers algorithm as below;
+#   (2) define all wins [[4, 9, 2], [3, 5, 7], ..., [2, 5, 8]] as constant, and check
+#       if any win set is subset of the given set.
+def win(s):
+    n = len(s)
+    if n < 3:
+        return False
+    s = sorted(s)
+    for i in range(n - 2):
+        l = i + 1
+        r = n - 1
+        while l < r:
+            total = s[i] + s[l] + s[r]
+            if total == 15:
+                return True
+            elif total < 15:
+                l = l + 1
+            else:
+                r = r - 1
+    return False
+
+def finished(x, o):
+    return len(x) + len(o) == 9
+
+def minmax(x, o, depth, maximize):
+    score = eval(x, o)
+    if score == WIN:
+        return score - depth
+    if score == -WIN:
+        return score + depth
+    if finished(x, o):
+        return 0  # draw
+    best = -INF if maximize else INF
+    for i in MAGIC_SQUARE:
+        if (i not in x) and (i not in o):
+            if maximize:
+                best = max(best, minmax([i] + x, o, depth + 1, not maximize))
+            else:
+                best = min(best, minmax(x, [i] + o, depth + 1, not maximize))
+    return best
+
+def findbest(x, o, maximize):
+    best = -INF if maximize else INF
+    move = 0
+    for i in MAGIC_SQUARE:
+        if (i not in x) and (i not in o):
+            if maximize:
+                val = minmax([i] + x, o, 0, not maximize)
+                if val > best:
+                    best = val
+                    move = i
+            else:
+                val = minmax(x, [i] + o, 0, not maximize)
+                if val < best:
+                    best = val
+                    move = i
+    return move
+
+# helper function
+def board(x, o):
+    for r in range(3):
+        print "---------"
+        for c in range(3):
+            p = MAGIC_SQUARE[r*3 + c]
+            if p in x:
+                print "|X",
+            elif p in o:
+                print "|O",
+            else:
+                print "| ",
+        print "|"
+    print "---------"
+
+# main program
+def play():
+    x = []
+    o = []
+    while not (win(x) or win(o) or finished(x, o)):
+        board(x, o)
+        i = 0
+        while True:
+            i = int(input("[1..9]==>"))
+            if i in x or i in o or (i not in MAGIC_SQUARE):
+                print "invalid move"
+            else:
+                x = [MAGIC_SQUARE[i-1]] + x
+                break
+        o = [findbest(x, o, False)] + o
+    board(x, o)
+
+play()
